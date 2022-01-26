3dc4cdd770da7675d9826b812b6101cdc0426c5b
Colin Spence <udappia@gmail.com>
2022-01-26 15:54:04 -0700

    Moves locked percentage from radial dial to meta.

--- packages/app/components/Balance.tsx
+++ packages/app/components/Balance.tsx
@@ -9,10 +9,6 @@ import { useTokenLockConfig } from "."
 import { BigNumber } from "ethers"
 import { formatUnits } from "ethers/lib/utils"
 import useTokenPrice from "./useTokenPrice"
-import clsx from "clsx"
-
-const CIRCLE_RADIUS = 26
-const CIRCUMFERENCE = CIRCLE_RADIUS * 2 * Math.PI
 
 export const formatToken = (bigNumber: BigNumber, decimals: number) =>
   new Intl.NumberFormat("en-US", {
@@ -48,12 +44,12 @@ const Balance: React.FC<Props> = ({ lockToken, ...rest }) => {
   const balanceLockToken = balanceLockTokenData as BigNumber | undefined
 
   const percentLocked =
-    balanceLockToken && balanceToken && balanceLockToken.gt(0)
-      ? balanceLockToken
-          .mul(100)
-          .div(balanceLockToken.add(balanceToken))
-          .toNumber()
-      : 0
+  balanceLockToken && balanceToken && balanceLockToken.gt(0)
+    ? balanceLockToken
+        .mul(100)
+        .div(balanceLockToken.add(balanceToken))
+        .toNumber()
+    : 0
 
   const balance = lockToken ? balanceLockToken : balanceToken
 
@@ -64,8 +60,15 @@ const Balance: React.FC<Props> = ({ lockToken, ...rest }) => {
     parseFloat(formatUnits(balance, decimals)) * tokenPrice
 
   return (
-    <Field {...rest}>
-      <div className={clsx(cls.wrapper, balance && lockToken && cls.hasLocked)}>
+    <Field
+      meta={balance && lockToken && (
+        <div className={cls.percentLockedText}>
+          {`${percentLocked}% of total GNO Locked`}
+        </div>
+      )}
+      {...rest}
+    >
+      <div className={cls.wrapper}>
         <div className={cls.icon}>
           <img
             src="/gno.svg"
@@ -86,38 +89,6 @@ const Balance: React.FC<Props> = ({ lockToken, ...rest }) => {
             </div>
           )}
         </div>
-        {balance && lockToken && (
-          <div className={cls.percentLockedWrapper}>
-            <svg
-              className={cls.percentLockedIndicator}
-              viewBox="0 0 56 56"
-              width={56}
-              height={56}
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <circle
-                className={cls.percentLockedCircle}
-                stroke="#001428"
-                strokeWidth="4"
-                fill="transparent"
-                r={CIRCLE_RADIUS}
-                cx="28"
-                cy="28"
-                style={{
-                  strokeDasharray: `${CIRCUMFERENCE} ${CIRCUMFERENCE}`,
-                  strokeDashoffset:
-                    CIRCUMFERENCE - (percentLocked / 100) * CIRCUMFERENCE,
-                }}
-              />
-            </svg>
-            {balanceToken && balanceLockToken && (
-              <>
-                <div className={cls.percentLockedAmount}>{percentLocked}%</div>
-                <div className={cls.percentLockedTitle}>Locked</div>
-              </>
-            )}
-          </div>
-        )}
       </div>
     </Field>
   )
